6d75f3d7f772109f3f08839c1ea485cbf1e2bd33
Starcatch <timurmishiev@gmail.com>
2021-02-18 01:52:40 -0500

    seed Burger and Restaurant data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   # # GET /users
-  # def index
-  #   @users = User.all
+  def index
+    @users = User.all
 
-  #   render json: @users
-  # end
+    render json: @users
+  end
 
   # # GET /users/1
   # def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# User.create(name: "Timur", username: "BurgerEater", email: "timur_mishiev@yahoo.com", password: "password" )
+
+# t.string "title"
+# t.string "url"
+
+# t.integer :restaurant_id
+# t.string :name
+# t.float :price
+# t.string :image
+# t.string :status
+# t.string :description
+
+# dumbo = Restaurant.create(title: "Burger Hub Dumbo", url: "burger-hub-dumbo")
+
+# burger1 = dumbo.burgers.create(name: "CheeseBurger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with pickes, and a fresh wheat bun.")
+
+# burger2 = dumbo.burgers.create(name: "ChickenBurger", price: 10.00, image: "/images/chicken.jpg", status: "available", description: "Chicken patty with melted American Cheese, BBQ souce,onion, tomato, and lettuce")
+
+# this data not seeded:
+# {
+# burger3 = dumbo.burgers.create(name: "Cheeseburger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with
+# pickes, and a fresh wheat bun.")
+
+# burger4 = dumbo.burgers.create(name: "Cheeseburger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with
+# pickes, and a fresh wheat bun.")
+
+# burger5 = dumbo.burgers.create(name: "Cheeseburger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with
+# pickes, and a fresh wheat bun.")
+
+# burger6 = dumbo.burgers.create(name: "Cheeseburger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with
+# pickes, and a fresh wheat bun.")
+
+# burger7 = dumbo.burgers.create(name: "Cheeseburger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with
+# pickes, and a fresh wheat bun.")
+
+# burger8 = dumbo.burgers.create(name: "Cheeseburger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with
+# pickes, and a fresh wheat bun.")
+
+# burger9 = dumbo.burgers.create(name: "Cheeseburger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with
+# pickes, and a fresh wheat bun.")
+# }
+
+# ditmaspark = Restaurant.create(title: "Burger Hub Ditmas Park", url: "burger-hub-ditmas-park")
+
+# burger1 = ditmaspark.burgers.create(name: "CheeseBurger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with pickes, and a fresh wheat bun.")
+
+# burger2 = ditmaspark.burgers.create(name: "ChickenBurger", price: 10.00, image: "/images/chicken.jpg", status: "available", description: "Chicken patty with melted American Cheese, BBQ souce,onion, tomato, and lettuce")
+
+
+
+# parkslope = Restaurant.create(title: "Burger Hub Park Slope", url: "burger-hub-park-slope")
+
+# burger1 = parkslope.burgers.create(name: "CheeseBurger", price: 12.00, image: "/image/cheese.jpg", status: "available", description: "Classic Burger  with  100%  grass-fed beef with pickes, and a fresh wheat bun.")
+
+# burger2 = parkslope.burgers.create(name: "ChickenBurger", price: 10.00, image: "/images/chicken.jpg", status: "available", description: "Chicken patty with melted American Cheese, BBQ souce,onion, tomato, and lettuce")
