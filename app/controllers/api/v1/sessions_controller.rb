a6a96df96f9e1c5191489a0f56e1fc00e15b9fca
Starcatch <timurmishiev@gmail.com>
2021-02-19 01:22:50 -0500

    add logged_in? method to Aplication controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,31 @@
 class SessionsController < ApplicationController
+
   def create
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else 
+      render json: {
+        error: "You Can't eat burgers"
+      }
+
+    end 
   end 
 
-  def 
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else 
+      render json: {
+        error: "No one logged in"
+      }
+    end 
   end 
-    
-  end
+end
+  
+
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-get "/api/v1/login", to: "sessions#create"
+post "/api/v1/login", to: "api/v1/sessions#create"
+get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
 # i can build a different log in for a burger control menu
   
   namespace :api do
