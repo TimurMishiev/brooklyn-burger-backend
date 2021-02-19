2d356dc8af8d56f6d10cdc3283505fa806a4f7e5
Starcatch <timurmishiev@gmail.com>
2021-02-19 02:46:12 -0500

    create functioning login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,31 +1,32 @@
-class SessionsController < ApplicationController
-
+class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:session][:username])
 
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      render json: @user
-    else 
+      render json: UserSerializer.new(@user), status: :ok
+    else
       render json: {
-        error: "You Can't eat burgers"
+        error: "Invalid Credentials"
       }
-
-    end 
-  end 
+    end
+  end
 
   def get_current_user
     if logged_in?
-      render json: current_user
-    else 
+      render json: UserSerializer.new(current_user)
+    else
       render json: {
         error: "No one logged in"
       }
-    end 
-  end 
-end
-  
+    end
+  end
 
-  
-  
+  def destroy
+    session.clear
+    render json: {
+      notice: "successfully logged out"
+    }, status: :ok
+  end
+
 end
