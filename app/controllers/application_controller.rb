47874a9fc985a9c404556ee8e529c80598a980fd
Starcatch <timurmishiev@gmail.com>
2021-03-02 20:39:26 -0500

    add user serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
-  # def current_user
-  #   User.find_by(id: session[:user_id])
-  # end
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 
-  # def logged_in?
-  #   !!current_user
-  # end 
+  def logged_in?
+    !!current_user
+  end 
 end
