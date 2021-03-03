3ecdae9e037043bd8984e600408d6571d4723763
Starcatch <timurmishiev@gmail.com>
2021-03-03 09:02:11 -0500

    fix seed data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-  #
-  # GET /users
+  
+  
   def index
     @users = User.all
 
@@ -13,46 +12,29 @@ class Api::V1::UsersController < ApplicationController
    
     user_json = UserSerializer.new(@user).serialized_json
     render json: user_json
+ 
+    
+        # render json: UserSerializer.new(@user).serializable_hash, status: :ok
+ 
+   
   end
 
-  # POST /users
-  def create
-    @user = User.new(user_params)
-    @location =  Location.find_or_create_by(city: params[:user][:hometown][:city], state: params[:user][:hometown][:state], country: params[:user][:hometown][:country])
+  
+  # def create
+  
+  # end
 
-    @user.hometown = @location
-    if @user.save
-      session[:user_id] = @user.id
-      render json: UserSerializer.new(@user), status: :created
-    else
-      resp = {
-        error: @user.errors.full_messages.to_sentence
-      }
-      render json: resp, status: :unprocessable_entity
-    end
-  end
+ 
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+
     def user_params
       params.require(:user).permit(:name, :username, :password)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,22 @@
 # t.string :status
 # t.string :description
 
-# dumbo = Restaurant.create(title: "Burger Hub Dumbo", url: "burger-hub-dumbo")
+dumbo = Restaurant.create(title: "Burger Hub Dumbo", url: "burger-hub-dumbo")
+
+burger1 = dumbo.burgers.create(name: "CheeseBurger", price: 12.00, image: "/images/cheese.jpg" , status: "available", description: "Classic Burger  with  100%  grass-fed beef with pickes, and a fresh wheat bun.")
+
+burger2 = dumbo.burgers.create(name: "ChickenBurger", price: 10.00, image: "/images/chicken.jpg" , status: "available", description: "Chicken patty with melted American Cheese, BBQ souce,onion, tomato, and lettuce")
+
+burger3 = dumbo.burgers.create(name: "Double Burger", price: 12.00, image: "/images/double.jpg", status: "available", description: "Two Beef patties with melted Cheddar and avocado." )
+
+burger4 = dumbo.burgers.create(name: "Fishburger", price: 13.50, image: "/images/fish.jpg", status: "available", description: "Grilled Fish fillet with lettuce, homemade light mayo, and pickles." )
+
+burger5 = dumbo.burgers.create(name: "Bison Burger", price: 15.00, image: "/images/bison.jpg", status: "available", description: " Wild bison patty, bacon, American Cheese, Mayo." )
+
+burger6 = dumbo.burgers.create(name: "Mega Burger", price: 14.00, image: "/images/mega.jpg", status: "available", description: "Two grass fed beef patties, cheddar, and pikles." )
+
+burger7 = dumbo.burgers.create(name: "Pita Burger", price: 11.00, image: "/images/pita.jpg", status: "available", description: "four small grass fed  beef patties , cheddar cheese, with lettuce, tomato, onion and freshly baked wheat pitta." )
+
 
 
 
@@ -50,6 +65,20 @@
 
 
 # parkslope = Restaurant.create(title: "Burger Hub Park Slope", url: "burger-hub-park-slope")
+
+# burger1 = parkslope.burgers.create(name: "CheeseBurger", price: 12.00, image: "/images/cheese.jpg" , status: "available", description: "Classic Burger  with  100%  grass-fed beef with pickles, and a fresh wheat bun.")
+
+# burger2 = parkslope.burgers.create(name: "ChickenBurger", price: 10.00, image: "/images/chicken.jpg" , status: "available", description: "Chicken patty with melted American Cheese, BBQ souce,onion, tomato, and lettuce")
+
+# burger3 = parkslope.burgers.create(name: "Double Burger", price: 12.00, image: "/images/double.jpg", status: "available", description: "Two Beef patties with melted Cheddar and avocado." )
+
+# burger4 = parkslope.burgers.create(name: "Fishburger", price: 13.50, image: "/images/fish.jpg", status: "available", description: "Grilled Fish fillet with lettuce, homemade light mayo, and pickles." )
+
+# burger5 = parkslope.burgers.create(name: "Bison Burger", price: 15.00, image: "/images/bison.jpg", status: "available", description: " Wild bison patty, bacon, American Cheese, Mayo." )
+
+# burger6 = parkslope.burgers.create(name: "Mega Burger", price: 14.00, image: "/images/mega.jpg", status: "available", description: "Two grass fed beef patties, cheddar, and pikles." )
+
+# burger7 = parkslope.burgers.create(name: "Pita Burger", price: 11.00, image: "/images/pita.jpg", status: "available", description: "four small grass fed  beef patties , cheddar cheese, with lettuce, tomato, onion and freshly baked wheat pitta." )
 
 
 
